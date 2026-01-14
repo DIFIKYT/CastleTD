ec489ea9e789e8163badc1e67ed416646208617e
павел тиунов <DIFIKYT>
2026-01-14 19:40:36 +0700

    Added main menu
    Removed lean localization, added sdk localization, started main menu

---
+++ Assets/Scripts/LangSwitcher.cs
@@ -0,0 +1,24 @@
+using com.cyborgAssets.inspectorButtonPro;
+using UnityEngine;
+using YG;
+
+public class LangSwitcher : MonoBehaviour
+{
+    [ProButton]
+    public void ChangeLanguageOnTurkish()
+    {
+        YG2.SwitchLanguage("tr");
+    }
+
+    [ProButton]
+    public void ChangeLanguageOnEnglish()
+    {
+        YG2.SwitchLanguage("en");
+    }
+
+    [ProButton]
+    public void ChangeLanguageOnRussian()
+    {
+        YG2.SwitchLanguage("ru");
+    }
+}
